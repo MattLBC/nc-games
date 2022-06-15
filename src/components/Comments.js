9376dc5e8fb921b56c83489a7bd61ed19efcf4e1
MattLBC <matt@Phoenix.localdomain>
2022-06-15 14:02:53 +0100

    Added non-functional buttons for adding a comment andvoting on ccoments

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -14,17 +14,28 @@ const Comments = ({ review_id }) => {
 
   return (
     <div className="commentsContainer">
-      <h1 className="commentTitle">Comments</h1>
+      <div className="commentBanner">
+        <h1 className="commentTitle">Comments</h1>
+        <button className="addComment">Add Comment</button>
+      </div>
       {comments.map((comment) => {
         return (
           <div className="commentCard" key={comment.comment_id}>
             <h3 className="commentUser">{comment.author}</h3>
             <p className="commentBody">{comment.body}</p>
             <p className="posted"> {`Posted: ${dayjs(comment.created_at)}`}</p>
-            <p className="commentVotes">
-              <span className="material-symbols-outlined">grade</span>{" "}
-              {comment.votes}
-            </p>
+            <div className="voteContainer">
+              <button className="upvote">
+                <span className="material-symbols-outlined">thumb_up</span>
+              </button>
+              <p className="commentVotes">
+                <span className="material-symbols-outlined">grade</span>{" "}
+                {comment.votes}
+              </p>
+              <button className="downvote">
+                <span className="material-symbols-outlined">thumb_down</span>
+              </button>
+            </div>
           </div>
         );
       })}
